1ab34f12bd447604f98c25f7af5513386de3f06e
Govin <govinbimsara@gmail.com>
2025-01-26 08:28:57 +0000

    Second Commit with app.py

---
+++ app.py
@@ -0,0 +1,22 @@
+import streamlit as st
+from langchain.chains import create_history_aware_retriever, create_retrieval_chain
+from langchain.chains.combine_documents import create_stuff_documents_chain
+from langchain_chroma import Chroma
+from langchain_community.chat_message_histories import ChatMessageHistory
+from langchain_core.chat_history import BaseChatMessageHistory
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from langchain_groq import ChatGroq
+from langchain_core.runnables.history import RunnableWithMessageHistory
+from langchain_huggingface import HuggingFaceEmbeddings
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+from langchain_community.document_loaders import PyPDFLoader
+import os
+
+from dotenv import load_dotenv
+load_dotenv()
+
+os.environ['HF_TOKEN']=os.getenv("HF_TOKEN")
+os.environ['LANGCHAIN_API_KEY']=os.getenv("LANGCHAIN_API_KEY")
+os.environ['LANGCHAIN_PROJECT']=os.getenv("LANGCHAIN_PROJECT")
+os.environ["LANGCHAIN_TRACING_V2"]="true"
+
